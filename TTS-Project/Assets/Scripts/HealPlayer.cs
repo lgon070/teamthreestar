6c7e1e5e12f4a30d6eee646f53ea4e9a64de6556
Groverino <johngrover9841@gmail.com>
2020-11-06 10:05:42 -0800

    Sound Effects and Animations

--- TTS-Project/Assets/Scripts/HealPlayer.cs
+++ TTS-Project/Assets/Scripts/HealPlayer.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class HealPlayer : MonoBehaviour
 {
 
     public int healingProvided = 25;
+
+    public GameObject pickupEffect;
+
+    public AudioSource Munch;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +29,10 @@ public class HealPlayer : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             FindObjectOfType<HealthManager>().HealPlayer(healingProvided);
+
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+
+            Munch.Play();
 
             Destroy(gameObject);
         }
