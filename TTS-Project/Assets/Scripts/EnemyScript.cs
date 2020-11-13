6d0db4ddaa8f11cafa69c1d2f06ec2f86cf24d9d
Luis <39094995+lgon070@users.noreply.github.com>
2020-11-13 14:51:38 -0800

    Assigment 7: Enemy AI (not really AI lets be honest)

---
+++ TTS-Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    Transform target;
+    public float speed = 1f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isWalking", true);
+        transform.LookAt(target.transform);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+}
