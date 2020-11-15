82e67707c11313996303ccaa7bb9758ad6e09f7e
Luis <39094995+lgon070@users.noreply.github.com>
2020-11-15 10:28:47 -0800

    enemy now has look radius

--- TTS-Project/Assets/Scripts/EnemyScript.cs
+++ TTS-Project/Assets/Scripts/EnemyScript.cs
@@ -7,7 +7,8 @@ using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     public Animator animator;
-    
+    public float lookRadius = 10f;
+
     // Start is called before the first frame update
     Transform target;
     public float speed = 1f;
@@ -20,10 +21,26 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("isWalking", true);
-        transform.LookAt(target.transform);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            animator.SetBool("isWalking", true);
+            transform.LookAt(target.transform);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        
+        
     }
 
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
 
 }
