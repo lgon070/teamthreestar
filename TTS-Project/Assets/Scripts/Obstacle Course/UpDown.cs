ca302b66bc3a9dcb0b22eacf2e8f479ae966656c
Groverino <johngrover9841@gmail.com>
2020-12-03 14:58:52 -0800

    Level_1/LevelChange

---
+++ TTS-Project/Assets/Scripts/Obstacle Course/UpDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    public bool Up;
+    public bool Down;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= 5.8)
+        {
+            Up = false;
+            Down = true;
+            
+        }
+        else if (transform.position.y <= 1.5)
+        {
+            Up = true;
+            Down = false;
+            
+        }
+
+        if (Down)
+        {
+            transform.position -= new Vector3(0, 2 * Time.deltaTime,0 );
+        }
+        if (Up)
+        {
+            transform.position += new Vector3(0, 2 * Time.deltaTime,0 );
+        }
+    }
+}
