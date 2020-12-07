117e204091cb5c7480a29d8a39c83c9be3a7eede
Luis <39094995+lgon070@users.noreply.github.com>
2020-12-06 18:38:25 -0800

    PLEASE for the love god dont make me do this again. What have i done to deserve such punishment

---
+++ TTS-Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyScript : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    public float chaseRadius = 8f;
+    public float rotationSpeed = 8f;
+    private Transform target;
+    private Animator animator;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance > lookRadius)
+        {
+            animator.SetBool("isChasing", false);
+            animator.SetBool("isAware", false);
+        }
+        if(distance <= lookRadius)
+        {
+            animator.SetBool("isChasing", false);
+            animator.SetBool("isAware", true);
+            FaceTarget();
+        }
+        if (distance <= chaseRadius)
+        {
+            animator.SetBool("isChasing", true);
+            animator.SetBool("isAware", false);
+            FaceTarget();
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+    }
+}
+
+
---
+++ TTS-Project/Assets/Scripts/TeleportCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCheck : MonoBehaviour
+{
+    public HealthManager Hman;
+    public AudioSource Ding;
+    public GameObject gameObject;
+    public Vector3 location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hman = FindObjectOfType<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            Ding.Play();
+            Hman.SetSpawnPoint(location);
+            DestroyObject(gameObject);
+
+        }
+    }
+}
